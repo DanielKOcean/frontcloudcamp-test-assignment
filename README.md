cc92203e6b0ecc7ee8e06cafc53279cce26ee2dc
Ivan Khorin <khorin.ivan.stefanovich@outlook.com>
2023-12-19 21:39:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FrontCloudCamp Test Assignment
+
+### Test assignment for admission to FrontCloudCamp.
+
+Official links:
+
+- [GitHub repository](https://github.com/FrontCloudCamp/test-assignment).
+- [Figma layout](https://www.figma.com/file/rzIp6awR6dGFVrcxcCEwzD/Untitled?type=design&node-id=0-1&t=90NCIZwzg7SIsdMb-0).
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>FrontCloudCamp Test Assignment</h1>
+      <p className="read-the-docs">
+        Test assignment for admission to FrontCloudCamp.
+      </p>
+      <p className="read-the-docs">
+        There will be changes according to <a href="https://www.figma.com/file/rzIp6awR6dGFVrcxcCEwzD/FrontCloudCamp">this Figma layout</a> instead of stub page soon.
+      </p>
+      <p className="read-the-docs">
+        🚀🚀🚀🚀🚀
+      </p>
+    </>
+  )
+}
+
+export default App
