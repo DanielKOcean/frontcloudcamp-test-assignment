d0a7b9614179231d989cf305c949ccc924aa8e16
Ivan Khorin <khorin.ivan.stefanovich@outlook.com>
2023-12-21 19:51:02 +0300

    Minor changes for local development

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/frontcloudcamp-test-assignment/", // For local development only. Use "build-gh-pages" for gh-pages deployment.
 })
