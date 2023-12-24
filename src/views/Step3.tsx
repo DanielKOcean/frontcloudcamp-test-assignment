2841536e090f11409aaf48000c4f094cccf9b333
Ivan Khorin <khorin.ivan.stefanovich@outlook.com>
2023-12-25 01:56:53 +0300

    Intermediate commit

--- src/views/Step3.tsx
+++ src/views/Step3.tsx
@@ -1,12 +1,33 @@
-import './Step3.css'
+import styles from './Step3.module.css'
 
 function Step3() {
   return (
-    <>
+    <div className={styles.container}>
       <div>
-        This is Step3 page 🚀🚀🚀🚀🚀
+        <ol className={styles.stepper}>
+          <li className={`${styles.stepperItem} ${styles.done}`}>
+            <p className={styles.stepperText}>1</p>
+          </li>
+          <li className={`${styles.stepperItem} ${styles.done}`}>
+            <p className={styles.stepperText}>2</p>
+          </li>
+          <li className={`${styles.stepperItem} ${styles.active}`}>
+            <p className={styles.stepperText}>3</p>
+          </li>
+        </ol>
       </div>
-    </>
+      <form className={styles.form}>
+        <div className={styles.formInputBlock}>
+          <label className={styles.formLabel} htmlFor="field-about">About</label>
+          <input className={styles.formControl} type="text" name="field-about" id="field-about" placeholder="Placeholder" />
+          <span className={styles.formTip}>Tip</span>
+        </div>
+      </form>
+      <div className={styles.buttons}>
+        <button id="button-back" className={`${styles.button} ${styles.secondary}`} type="button">Назад</button>
+        <button id="button-send" className={`${styles.button} ${styles.primary}`} type="button">Отправить</button>
+      </div>
+    </div>
   )
 }
 
